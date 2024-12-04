794d2a2c6deba0d08e78ae9acb39428a4d615be7
mfun.dev <personal.unknown.101@proton.me>
2024-12-04 11:55:08 -0500

    Initial Commit

---
+++ AquaMate/AquaMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaMateApp.swift
+//  AquaMate
+//
+//  Created by Marcos Fundora on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AquaMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
